a12b1c0d476be4648c50990a9752e3fd77c2e4ff
maxgrier <max.grier1@gmail.com>
2024-05-11 14:40:09 -0700

    Updating address

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,17 +31,20 @@ class About extends Component {
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
+						   <span>{name}</span>
+                     <br />
+						   {/* <span>{street}
+                     <br />
 						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
+                   </span>
+                   <br /> */}
+						   <span>{phone}</span>
+                     <br />
                      <span>{email}</span>
 					   </p>
                </div>
                <div className="columns download">
                   <p>
-                  {/* <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a> */}
                   <a target="_blank" rel="noopener noreferrer" href={resume} download className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
